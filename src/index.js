d2b393adeb6cdfebe3a224f807829529b30faf3f
turwokts <48319429+TEKKSUNN@users.noreply.github.com>
2024-10-07 13:49:48 +0800

    Make dropdown an actual dropdown

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import "./stylesheets/styles.css";
+
+import { loadDropDown } from "@tekksunn/drop-down";
+
+document.addEventListener("DOMContentLoaded", () => {
+  loadDropDown("div.dropdown-content", "hover");
+});
