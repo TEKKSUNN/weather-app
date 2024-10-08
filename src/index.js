cef307d22fa908a1b3433dd59516b2612ae2e756
turwokts <48319429+TEKKSUNN@users.noreply.github.com>
2024-10-08 16:55:19 +0800

    Automatically show location status

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import updateLocationStatus from "./modules/dom/location-status";
 import { assignCurrentLocation } from "./modules/location";
 import "./stylesheets/styles.css";
 
@@ -6,6 +7,7 @@ import { loadDropDown } from "@tekksunn/drop-down";
 function main() {
   loadDropDown("div.dropdown-content", "hover");
   assignCurrentLocation();
+  updateLocationStatus();
 }
 
 document.addEventListener("DOMContentLoaded", main);
