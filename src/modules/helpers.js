1e1ed1bee63fb6948cc9c016b5450f473718e1dc
turwokts <48319429+TEKKSUNN@users.noreply.github.com>
2024-10-09 18:32:58 +0800

    Make createDiv fn & createElement shorthand

--- src/modules/helpers.js
+++ src/modules/helpers.js
@@ -9,3 +9,13 @@ export function onclickOf(HTMLElement, callbackfn) {
 export const getWeatherCardsDiv = () => document.getElementById("weather-cards");
 
 export const getModeButtons = () => Array.from(document.querySelectorAll("button.mode"));
+
+const create = (elementType) => document.createElement(elementType);
+
+export function createDiv(className) {
+  const div = create("div");
+  if (className) {
+    div.className = className;
+  }
+  return div;
+}
