43ce2625ac4013d0aa3264d23c46ed33fe8d0311
turwokts <48319429+TEKKSUNN@users.noreply.github.com>
2024-10-09 15:58:32 +0800

    Export fn to get weather-cards div

--- src/modules/helpers.js
+++ src/modules/helpers.js
@@ -1,3 +1,5 @@
 export function stopSubmitEvent(HTMLElement) {
   HTMLElement.addEventListener("submit", (e) => { e.preventDefault() });
 }
+
+export const getWeatherCardsDiv = () => document.getElementById("weather-cards");
