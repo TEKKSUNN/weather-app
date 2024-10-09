54bd09ee403a600288567960c08f2619e25b7752
turwokts <48319429+TEKKSUNN@users.noreply.github.com>
2024-10-09 16:33:32 +0800

    Make onlickOf fn for ez callback application

--- src/modules/helpers.js
+++ src/modules/helpers.js
@@ -2,6 +2,10 @@ export function stopSubmitEvent(HTMLElement) {
   HTMLElement.addEventListener("submit", (e) => { e.preventDefault() });
 }
 
+export function onclickOf(HTMLElement, callbackfn) {
+  HTMLElement.addEventListener("click", callbackfn);
+}
+
 export const getWeatherCardsDiv = () => document.getElementById("weather-cards");
 
 export const getModeButtons = () => Array.from(document.querySelectorAll("button.mode"));
