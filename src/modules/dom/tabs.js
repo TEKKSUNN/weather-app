1ab2c413f8b25e75b4b370e70829be65a80bc706
turwokts <48319429+TEKKSUNN@users.noreply.github.com>
2024-10-09 16:15:17 +0800

    Export fn for assigning tab focused

--- src/modules/dom/tabs.js
+++ src/modules/dom/tabs.js
@@ -1,3 +1,5 @@
+import { loadMaximumDepth, loadNormalDepth } from "./modes";
+
 const modeButtons = Array.from(document.querySelectorAll("li.dropdown-item"));
 const focusClass = "focused";
 
@@ -14,10 +16,33 @@ function deactivateTabs() {
   modeButtons.forEach(deactivateTab);
 }
 
+function activateTabFocused(tabFocused) {
+  if (tabFocused === "normal") {
+    loadNormalDepth();
+  } else {
+    loadMaximumDepth();
+  }
+}
+
+async function setTabFocused(tabName) {
+  localStorage.setItem("tabFocused", tabName);
+}
+
 export function activateNormalTab() {
   activateTab(0);
+  setTabFocused("normal");
 }
 
 export function activateMaximumTab() {
   activateTab(1);
+  setTabFocused("maximum");
+}
+
+export function assignTabFocused() {
+  const tabFocused = localStorage.getItem("tabFocused");
+  if (tabFocused) {
+    activateTabFocused(tabFocused);
+  } else {
+    setTabFocused("normal");
+  }
 }
