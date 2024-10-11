5c0b428b97e5903d2089a1c827989c7a67f5869a
turwokts <48319429+TEKKSUNN@users.noreply.github.com>
2024-10-11 21:10:20 +0800

    Update weather cards also

--- src/modules/update.js
+++ src/modules/update.js
@@ -1,7 +1,9 @@
 import updateLocationStatus from "./dom/location-status";
+import { updateWeatherCards } from "./dom/weather-cards";
 import updateWeatherData from "./weather";
 
 export default async function updateEverything() {
   updateLocationStatus();
-  updateWeatherData();
+  await updateWeatherData();
+  updateWeatherCards();
 }
